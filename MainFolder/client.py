8d6d481cb0b703a1367e603826d168f06a8d20e3
heesooyaam <mr.filonets-anton@yandex.ru>
2023-12-07 21:52:17 +0300

    skldfkladf

--- MainFolder/client.py
+++ MainFolder/client.py
@@ -1,8 +1,12 @@
 import socket
 
-def GET(column_name, table_name):
+def GET():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('127.0.0.1', 2000))
+    print('Print table_name')
+    table_name = str(input())
+    print('Chose column')
+    column_name = input()
     get = f"SELECT {column_name} FROM {table_name}"
     sock.send(get.encode('utf8'))
     data = sock.recv(1024)
@@ -10,10 +14,18 @@ def GET(column_name, table_name):
 
     print('I found: ', data)
 
-def CREATE(table_name, columns):
+def CREATE():
+    print('Print table_name')
+    table_name = str(input())
+    print('input column names and "0" in the end')
+    columns = input()
+    cur = input()
+    while cur != '0':
+        columns += ', ' + cur
+        cur = input()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('127.0.0.1', 2000))
-    create = f"CREATE TABLE {table_name} ({columns})"
+    create = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
     sock.send(create.encode('utf8'))
     data = sock.recv(1024)
     print(data.decode('utf8'))
@@ -25,22 +37,39 @@ def CREATE(table_name, columns):
 def SET():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('127.0.0.1', 2000))
+    print('Print table_name')
+    table_name = str(input())
     set = f"INSERT INTO {table_name} (id, name, surname, birthdate) VALUES (2, 'Petr', 'Ivanov', '2003-12-13') "
     sock.send(set.encode('utf8'))
     data = sock.recv(1024)
     sock.close()
 
     print('I post: ', data)
+def SET_FILE():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('127.0.0.1', 2000))
+    print('Print table_name')
+    table_name = str(input())
+    print("Print name of your file and path to it")
+    fileName = input()
+    filePath = input()
+    with open(filePath, 'rb') as file:
+        file_data = file.read()
+    setFile = f"INSERT INTO {table_name} (filename, data) VALUES ({fileName}, {file_data}) "
 
-print('Print table_name')
-# column_name = str(input())
-table_name = str(input())
-print('input column names and "0" in the end')
-columns = input()
-cur = input()
-while cur != '0':
-    columns += ', ' + cur
-    cur = input()
-# GET(column_name, table_name)
-CREATE(table_name, columns)
 
+actions = ['CREATE_TABLE', 'SET_DATA', "GET_DATA"]
+print('Chose action:\n')
+print(actions)
+action = input()
+if action == 'CREATE_TABLE':
+    CREATE()
+elif action == 'SET_DATA':
+    print('Do you want to add file?\nYes/No')
+    ans = str(input())
+    if ans == 'YES':
+        SET_FILE()
+    else:
+        SET()
+else:
+    GET()
